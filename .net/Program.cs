3dd44489a66cfacfba0caa56666c20786f6b2292
y10 <serhiy.voytenko@gmail.com>
2022-02-10 19:27:32 -0500

    added merge sorted lists

--- .net/Program.cs
+++ .net/Program.cs
@@ -2,13 +2,27 @@
 {
     class Program
     {
+        public static void TestMostWordyUsers()
+        {
+            Utils.Assert(() => new MostWordyUsers.Solution("./most-wordy-users/list.txt", 3).Run(), (r) =>
+               r.Count == 3 &&
+               (r["username4"]?.Equals(16) ?? false) &&
+               (r["username3"]?.Equals(8) ?? false) &&
+               (r["username"]?.Equals(7) ?? false));
+        }
+
+        public static void TestMergeSortedLists()
+        {
+            Utils.Testit(() => new MergeSortedLists.Solution(filename: "./merge-sorted-lists/lists.txt"))
+                 .Mesure((test) => test.UseNativeSort())
+                 //.Mesure((test) => test.UseBruteForce())
+                 .Mesure((test) => test.UsePriorityQueue());
+                 //.Mesure((test) => test.DevideAndConquer());
+        }
+
         public static void Main(string[] args)
         {
-            Utils.Testit(() => new MostWordyUsers.Solution("./most-wordy-users/list.txt", 3).Run(), (r) =>
-                r.Count == 3 &&
-                (r["username4"]?.Equals(16) ?? false) &&
-                (r["username3"]?.Equals(8) ?? false) &&
-                (r["username"]?.Equals(7) ?? false));
+            TestMergeSortedLists();
         }
     }
 }
